a74244e8fcdc065ea6448b510caf1f239ee385c3
M.Elavarasan <31089822+sachinelavarasan@users.noreply.github.com>
2022-07-26 23:25:44 +0530

    Update index.js

--- hotel-api/routes/index.js
+++ hotel-api/routes/index.js
@@ -1,18 +1,15 @@
 const auth = require('./auth');
-const webhook = require('./webhook');
 
 const profile = require('./profile');
 
 const user = require('./api/user');
 const hotel = require('./api/hotel');
-// const comments = require('./api/comments');
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
     res.send('Hotel');
   });
   app.use('/auth', auth);
-  app.use('/webhook', webhook);
 
   app.use('/profile', profile);
 
